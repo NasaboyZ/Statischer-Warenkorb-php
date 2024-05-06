f8a72add863ca4d18af98912e5f318ac7449357b
NasaboyZ <josefleite.00@hotmail.com>
2024-05-06 10:48:10 +0200

    private and public methods for the functionality of the application and the application itself

--- 11-objektorientierung/04-private.php
+++ 11-objektorientierung/04-private.php
@@ -2,13 +2,13 @@
 
 class BankAccount{
 
-    function __construct(private string $iban, private float $balance){}
+    public function __construct(private string $iban, private float $balance){}
 
-    function printBalance(){ // Methode
+    private function printBalance(){ // Methode
         echo "Der Kontostand von {$this->iban} ist: {$this->balance} Euro\n";
     }
 
-    function sepa(BankAccount $to, float $amount){
+     public function sepa(BankAccount $to, float $amount){
         if($this->balance < $amount){
             echo "Kontostand zu niedrig für Überweisung\n";
             return;
@@ -18,17 +18,21 @@ class BankAccount{
         $to->balance += $amount; // $to->balance = $to->balance + $amount
     }
 
-    function getIban(){
+    public function getIban(){
         return $this->iban;
     }
-    function getBalance(){
+    public function getBalance(){
         return $this->balance;
+    }
+    public function atmPayIn($amount){
+        $this->balance+= $amount;
+        $this->printBalance();
     }
 }
 
 $account1 = new BankAccount('DE12345679153', 750); // erstellt eine neue Instanz der Klasse BankAccount mit IBAN 'DE12345679153' und einem Kontostand von 750 Euro
 $account2 = new BankAccount('CH70003333949393', 8399); // erstellt eine neue Instanz der Klasse BankAccount mit IBAN 'CH70003333949393' und einem Kontostand von 8399 Euro
-
+$account1 ->atmPayIn(10000);
 var_dump($account1 -> getIban());
 var_dump($account1 -> getBalance());
 // var_dump($account1-> balance);
